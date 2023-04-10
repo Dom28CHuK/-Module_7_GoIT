a8815704acefbd702608d2f0b9703a5484f218e7
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-10 14:35:06 +0300

    add new class Example

--- java/Main.java
+++ java/Main.java
@@ -1,24 +1,16 @@
-import classes.GooSearchResult;
-import classes.GooWordStat;
+import classes.Examples;
 
 public class Main {
     public static void main(String[] args) {
-//        //Word is [human], search freq is LOW
-//        System.out.println(new GooWordStat("human", 10));
-//
-//        //Word is [mars], search freq is MEDIUM
-//        System.out.println(new GooWordStat("mars", 10000));
-//
-//        //Word is [space], search freq is HIGH
-//        System.out.println(new GooWordStat("space", 9965499));
-//
-//        //Word is [life], search freq is EXTRA HIGH
-//        System.out.println(new GooWordStat("life", 564785654));
+        Examples examples = new Examples();
 
-        //test.com
-        System.out.println(new GooSearchResult("https://test.com").parseDomain());
+        String reversed = Examples.reverseStringWithNewObject("Hello World");
+        System.out.println(reversed);
 
-        //apple.in.mars
-        System.out.println(new GooSearchResult("http://apple.in.mars").parseDomain());
+        String reversed1 = Examples.reverseStringWithoutAdditionalMemoryUsage("Hello World!");
+        System.out.println(reversed1);
+
+        String reversed2 = Examples.stringBuilderReverseExample("Hello, World!");
+        System.out.println(reversed2);
     }
 }
