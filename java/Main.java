a7053b627a161908edafebcfb01dcc4112a53f7b
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-11 10:21:16 +0300

    update

--- java/Main.java
+++ java/Main.java
@@ -1,12 +1,17 @@
-import classes.Examples;
-import classes.GooSearchResult;
+import classes.Phrase;
 
 public class Main {
     public static void main(String[] args) {
-        //test.com
-        System.out.println(new GooSearchResult("https://test.com").parseDomain());
+        String[] words = {"alpha", "beta", "gamma"};
 
-        //apple.in.mars
-        System.out.println(new GooSearchResult("http://apple.in.mars").parseDomain());
+        Phrase phrase = new Phrase(words);
+
+        //alpha beta gamma
+        System.out.println(phrase);
+
+        words[0] = "zero";
+
+        //alpha beta gamma
+        System.out.println(phrase);
     }
 }
