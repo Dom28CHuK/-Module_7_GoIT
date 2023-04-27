b61e241ea241664f42d8b216c9db879ce3ec37bc
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-27 08:44:09 +0300

    MathDetector

--- java/Main.java
+++ java/Main.java
@@ -1,40 +1,14 @@
-import classes.DigitText;
-import classes.SensitiveDataSearcher;
-import classes.SummaryCreator;
-import classes.WordDeleter;
+import classes.MathDetector;
 
 public class Main {
     public static void main(String[] args) {
-//        WordDeleter wordDeleter = new WordDeleter();
-//
-//        //Hello
-//        System.out.println(wordDeleter.remove("Hello Java", new String[] {"Java"}));
-//
-//        //This Sparta
-//        System.out.println(wordDeleter.remove("This is Sparta", new String[] {"is"}));
+        //true
+        System.out.println(new MathDetector().isMath("2+2=4"));
 
-//        SensitiveDataSearcher searcher = new SensitiveDataSearcher();
-//
-//        //false
-//        System.out.println(searcher.isSensitiveDataPresent("Hello world"));
-//
-//        //true
-//        System.out.println(searcher.isSensitiveDataPresent("Pass: swordfish"));
+        //false
+        System.out.println(new MathDetector().isMath("1992 is great year"));
 
-//        //true
-//        System.out.println(new DigitText().detect("23 50"));
-//
-//        //false
-//        System.out.println(new DigitText().detect("Year 1990"));
-
-        SummaryCreator summaryCreator = new SummaryCreator();
-
-        //Mars
-        String shortPhrase = "Mars";
-        System.out.println(summaryCreator.create(shortPhrase));
-
-        //Java is very po...
-        String longPhrase = "Java is very popular language";
-        System.out.println(summaryCreator.create(longPhrase));
+        //false
+        System.out.println(new MathDetector().isMath("Venus vs Earth"));
     }
 }
