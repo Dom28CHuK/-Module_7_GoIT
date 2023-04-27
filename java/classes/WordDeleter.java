b57932a874274a87da0686b015c7272722415b9b
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-27 07:40:20 +0300

    WordDeleter

--- java/classes/WordDeleter.java
+++ java/classes/WordDeleter.java
@@ -2,7 +2,21 @@ package classes;
 
 public class WordDeleter {
     public String remove(String phrase, String[] words) {
-        String[]
-        
+        String[] phraseArray = phrase.split(" ");
+        String result = "";
+        for (int i = 0; i < phraseArray.length; i++) {
+            String currentWord = phraseArray[i];
+            boolean isWordToRemove = false;
+            for (int j = 0; j < words.length; j++) {
+                if (currentWord.equals(words[j])) {
+                    isWordToRemove = true;
+                    break;
+                }
+            }
+            if (!isWordToRemove) {
+                result += currentWord + " ";
+            }
+        }
+        return result.trim();
     }
 }
